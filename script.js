26c6c7cd9030da6587b6c5901672bbe6bfdb6a91
jeeko20 <jeekothebest@gmail.com>
2025-02-06 20:24:15 -0500

    ajout de l'index html

---
+++ script.js
@@ -0,0 +1,11 @@
+// Animation au scroll
+window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset >= sectionTop - sectionHeight / 3) {
+            section.classList.add('show');
+        }
+    });
+});
